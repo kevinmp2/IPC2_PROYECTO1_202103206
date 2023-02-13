2ced497e1300cf372d56d40673576c0af39a49a8
kevinmp2 <3034573840109@ingenieria.usac.edu.gt>
2023-02-12 21:02:40 -0600

    Primer commit

---
+++ nodo.py
@@ -0,0 +1,18 @@
+class Nodo():
+    def __init__(self, valor = None, id = -1):
+        self.valor = valor
+        self.siguiente = None
+        self.id = id
+
+    def get_valor(self):
+        return str(self.valor)
+        
+    
+    def set_valor(self, valor):
+        self.valor = valor
+    
+    def get_sigueinte(self):
+        return self.siguiente
+    
+    def set_siguiente(self, siguiente):
+        self.siguiente = siguiente
