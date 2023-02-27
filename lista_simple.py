857ef0bed2976685781f693417ce0174378ba43c
kevinmp2 <3034573840109@ingenieria.usac.edu.gt>
2023-02-26 20:22:44 -0600

    Segundo release

---
+++ lista_simple.py
@@ -0,0 +1,24 @@
+from nodo import Nodo
+
+class Lista_simple:
+    
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+
+    def insertar(self, dato):
+        nodo = Nodo(dato)
+        if self.primero is None:
+            self.primero = nodo
+            self.ultimo = self.primero
+        else:
+            self.ultimo.siguiente = nodo
+            self.ultimo = nodo
+    
+    def imprimir(self):
+        actual = self.primero
+        while actual:
+            print(actual.dato.codigo)
+            actual = actual.siguiente
+
+
---
+++ objetos.py
@@ -0,0 +1,21 @@
+from lista_simple import Lista_simple
+
+
+class Organismo():
+    def __init__(self, codigo, nombre):
+        self.codigo = codigo
+        self.nombre = nombre
+    
+class Muestra():
+    def __init__(self, codigo, descripcion, filas, columnas):
+        self.codigo = codigo
+        self.descripcion = descripcion
+        self.filas = filas 
+        self.columnas = columnas
+        self.lista_celdas_vivas = Lista_simple()
+
+class Celda():
+    def __init__(self, fila, columna, codigo_organismo):
+        self.fila = fila 
+        self.columna = columna
+        self.codigo_organismo = codigo_organismo
